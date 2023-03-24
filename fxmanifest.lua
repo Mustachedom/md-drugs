b3ae62f81c0d77edd1e1f80d929e8ad55ab2effc
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2023-03-24 09:59:59 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,37 @@
+name "mustache-drugs"
+author "mustache_dom"
+description "drugs by mustache dom"
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+	
+	'client/cocaine.lua',
+	'client/heroin.lua',
+	'client/lsd.lua',
+	'client/target.lua',
+	'client/crack.lua',
+	'client/shrooms.lua',
+	'client/pharma.lua',
+	'client/mission.lua'
+}
+
+server_scripts {
+    'server/cocaine.lua',
+	'server/heroin.lua',
+	'server/lsd.lua',
+	'server/crack.lua',
+	'server/shrooms.lua',
+	'server/pharma.lua',
+	'server/mission.lua',
+}
+
+shared_scripts {
+    'config.lua',
+}
+
+
+
+lua54 'yes'
+
+dependency '/assetpacks'
