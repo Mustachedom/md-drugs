bbed82e3e16981d05867a6b19aabe96d8162936d
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-01-08 23:55:31 -0800

    i missed a ''

--- server/mescaline.lua
+++ server/mescaline.lua
@@ -43,7 +43,7 @@ local Player = QBCore.Functions.GetPlayer(src)
 
  if Player.Functions.RemoveItem("cactusbulb", 1) then
 		Player.Functions.AddItem("driedmescaline", 1)
-		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[driedmescaline], "add", 1)
+		TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['driedmescaline'], "add", 1)
 else
 	TriggerClientEvent('QBCore:Notify', src, "No Cactus Bulbs", "error")
 end
