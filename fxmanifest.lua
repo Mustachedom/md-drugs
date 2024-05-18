9d76824013e73fbf270edf5d0e661a1039337907
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-05-18 13:40:22 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ author "mustache_dom"
 description "drugs by mustache dom"
 fx_version "cerulean"
 game "gta5"
-version  '3.5.3'
+version  '3.5.4'
 client_scripts {
 	'client/**.lua',
 	'@PolyZone/client.lua',
