192e8c9d6449e47a6ed914b7ed392a89fed7fa75
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-07-11 17:42:05 -0700

    Update lean.lua

--- client/lean.lua
+++ client/lean.lua
@@ -4,8 +4,6 @@ CreateThread(function()
 	local current = "s_m_m_doctor_01"
 	lib.requestModel(current, Config.RequestModelTime)
 	local SyrupLocation = CreatePed(0, current,Config.SyrupVendor.x,Config.SyrupVendor.y,Config.SyrupVendor.z-1, Config.SyrupVendor.w, false, false)
-    --FreezeEntityPosition(SyrupLocation, true)
-   -- SetEntityInvincible(SyrupLocation, true)
     Freeze(SyrupLocation, true, Config.SyrupVendor.w)
     local options = {
         { type = "client", label = "Get Task", icon = "fas fa-eye", event = "md-drugs:client:getsyruplocationtobuy", distance = 2.0},
