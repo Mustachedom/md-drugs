971168c9b6dc364cd6d7a6bd2f03225415f6f2af
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-07-11 17:45:55 -0700

    Update travellingmerchant.lua

--- client/travellingmerchant.lua
+++ client/travellingmerchant.lua
@@ -5,8 +5,6 @@ local current = "g_m_y_famdnf_01"
 	lib.requestModel(current, Config.RequestModelTime)
 	local CurrentLocation = Config.Travellingmerchant[math.random(#Config.Travellingmerchant)]
 	local travellingmerchant = CreatePed(0, current,CurrentLocation.x,CurrentLocation.y,CurrentLocation.z-1, CurrentLocation.w, false, false)
-    --FreezeEntityPosition(travellingmerchant, true)
-    --SetEntityInvincible(travellingmerchant, true)
 	Freeze(travellingmerchant, true, CurrentLocation.w)
 	if Config.oxtarget then
 		local options = {
