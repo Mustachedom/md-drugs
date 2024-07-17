99c2f2d2121e90939733e6a3dfec938983b748d2
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-07-17 09:33:25 +0200

    Create danish.lua

---
+++ langs/danish.lua
@@ -0,0 +1,167 @@
+Lang = {}
+
+Lang.Coke = { 
+    -- klient side progressbarer
+    picking = "Plukker Blade",
+    makepow = 'Hakker Blade Til Pulver',
+    cutting = 'Blander Kokain Med Bagepulver',
+    bagging = "Pakker Denne Lort",
+    -- slut progressbarer
+    cut = "Du Har Skåret Noget Kokain",
+    nocutcoke = "Du Kan Ikke Skære Kokain Uden Kokain, Idiot",
+    bagcoke = "Du Har Pakket Kokain",
+    nobagcoke = "Du Kan Ikke Pakke Kokain Uden Kokain, Idiot",
+}
+Lang.Crack = {
+    --klient side
+    cookcrack = "Koger Noget Crack",
+    bagcrack = 'Pakker Denne Lort',
+    -- server
+    makecrack = 'Godt Crack',
+    nocoke = 'Du Kan Ikke Lave Dette Uden Kokain'
+}
+Lang.Cornerselling = {
+    -- klient side beskeder
+    toofar = "For Langt Væk Fra Dit Hjørne, Homie",
+    inacar = "Du Kan Ikke Sælge Mens Du Er I En Bil",
+    deny = "Fuck Det Tilbud",
+    start = "Tjen De Penge",
+    stop = "Doven Røv Kan Ikke Engang Sælge Stoffer",
+    nodrugs = "Hvad Vil Du Sælge? Luft?",
+    notcops = "Ikke Nok Betjente",
+    -- slut klient side beskeder
+    -- progressbarer
+    sell = "Sælger Stoffer",
+}
+Lang.Delivery = {
+    -- klient side
+    search = "Søger Efter En Levering ...",
+    none = "Intet I Nærheden",
+    busy = "Du Har Ikke Engang Færdiggjort Den Jeg Sagde Til Dig",
+    found = "Markeret På Dit Kort",
+    already = "Allerede Levering",
+    checkrep = "Nuværende Forhandler Repræsentation: ",
+    -- progressbarer
+    pack = 'Overrækker Pakke',
+    -- slut klient side
+}
+
+Lang.Heroin = {
+    --klient
+    pick = "Plukker Valmuer",
+    tableout = 'Du Har Allerede Et Bord Ude',
+    dryout = "Tørrer Valmuer",
+    cutting = "Blander Heroin Med Bagepulver",
+    secret = "SHHHHHHHHH",
+    table = "Sætter Bord Ned",
+    success = "Opvarmede Heroin Rigtigt",
+    clean = 'Rengør Heroin Bord',
+    needles = 'Fylder Sprøjte',
+    pickup = 'Tager Heroin Bord Op',
+    -- 
+    cutheroin = 'Du Lavede Noget Blandingsheroin',
+    noheroin = "Hvordan Kan Du Skære Heroin Uden Heroin, Idiot",
+    vial = 'Du Lavede Et Vial Med Heroin',
+    novial = "Hvordan Kan Du Lave Heroin Vialer Uden Heroin, Idiot",
+    fail = 'Du Overophedede Dem, Idiot',
+    fill = 'Fyldte Sprøjten',
+    nofill = 'Hvordan Vil Du Fylde Sprøjter Med Heroin Uden Heroin'
+}
+Lang.Lean = {
+    marked = 'Bil Er Markeret På Dit Kort',
+    lean = 'Du Har Purple Lean',
+    dextro = 'Du Har Rød Dextro'
+}
+
+Lang.lsd = {
+    steallys = 'Stjæler Lyserginsyre',
+    stealdie = 'Stjæler Lyserginsyre',
+    tableout = 'Du Har Allerede Et Bord Ude',
+    tablepack = 'Pakker Bord',
+    heat = 'Opvarmer Væsker',
+    clean = 'Rengør Bordet',
+    refine = 'Forfiner Kvaliteten',
+    dip = 'Dypper LSD På Papir',
+    buypaper = 'Køber Tab Papir',
+    buykit = 'Køber Lab Kit',
+    place = 'Sætter Bord Ned',
+    -- server
+    lys = 'Du Har Lyserginsyre',
+    die = 'Du Har Diethylamid',
+    cleaned = 'Du Rengjorde Det Perfekt',
+    madelsd = 'Du Lavede LSD!',
+    failed = 'Du Overophedede Dem, Idiot',
+    refined = 'Du Forfinede Dem!',
+    broke = 'Din Fattige Røv Har Ikke Råd Til Disse!',
+    novial = 'Ingen LSD Vialer, Idiot',
+}
+Lang.mescaline = {
+    pick = "Plukker Kaktus",
+    dry = 'Tørrer Kaktus',
+    nocactus = 'Ingen Kaktus Bulbs',
+    eat = 'Spiser Mescaline'
+}
+
+Lang.meth = {
+    inside = 'Ting Indeni Allerede',
+    increaseheat = 'Øger Varme',
+    mission = 'Bil Er Markeret På Dit Kort',
+    ace = 'Du Har Noget Aceton',
+    eph = 'Du Har Noget Efedrin',
+    mix = 'Tilsætter Ting Til Blandingen',
+    nobag = 'Ingen Poser'
+}
+
+Lang.oxy = {
+    truck = 'Din Lastbil Er Her',
+    cantcarry = 'Du Er Ikke Så Stærk, Du Kan Kun Bære En',
+    hand = 'Overrækker Oxy',
+    empty = 'Du Kan Ikke Være Tomhændet, Idiot',
+    broke = 'Din Fattige Røv Har Ikke Råd Til Dette',
+    hot = 'Bilen Er Hot, Dump Den Nu',
+    next = 'Kom Til Den Næste Lokation'
+}
+
+Lang.Pharma = {
+    notems = 'Du Er Ikke En Medicinsk Person',
+    write = 'Skriver Recept',
+    fill = 'Fylder Recept',
+    open = 'Åbner Flaske',
+    pills = 'Du Har Nogle Piller!',
+    no = 'Du Har Ingen Recept'
+}
+
+Lang.Shrooms = {
+    pick = 'Plukker Svampe',
+    eat = 'Spiser Svampe',
+    trip = 'Hav En God Tur!'
+}
+
+Lang.Weed = {
+    pick = 'Høster Weed',
+    busy = 'Tørrer Allerede En Ud',
+    canna = 'Laver Cannabis-smør',
+    brown = 'Laver Specielle Brownies',
+    cook = 'Laver Specielle Cookies',
+    Choc = 'Laver Speciel Chokolade',
+    muff = 'Laver Speciel Muffin',
+    stovehot = 'Komfuret Er For Varmt, Vent 30 Sekunder På At Køle Ned',
+    shat = 'Laver Shatter',
+    roll = 'Ruller Blunts',
+    -- 
+    nodry = 'Du Har Brug For Våd Cannabis Til At Tørre',
+    noweed = 'Kan Ikke Rulle En Blunt Med Luft',
+    noshat = 'Du Har Brug For Shatter Til At Lave En Dab',
+    notorch = 'Du Har Brug For En Butanbrænder'
+}
+
+Lang.xtc = {
+    out = 'Du Har Allerede En Presser Ude',
+    pressing = 'Presser Piller',
+    raw = 'Laver Rå XTC',
+    mdp2p = 'Stjæler MDP2P',
+    iso = 'Stjæler Isosafrol',
+    fail = 'Dude, Det Er En Cirkel, Ikke Raketteknologi',
+    --
+    cash = 'Jeg har brug for 20k i kontanter'
+}
