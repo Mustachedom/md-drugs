37ba74a700680cb802961dbc4190fbef92df2674
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-09-28 09:58:56 -0700

    Update functions.lua

--- client/functions.lua
+++ client/functions.lua
@@ -359,6 +359,17 @@ function AddBoxZoneMulti(name, table, data)
 	end
 end
 
+function AddBoxZoneMultiOptions(name, table, data) 
+
+		if Config.Target == 'qb' then
+			exports['qb-target']:AddBoxZone(name .. k, v.loc, 1.5, 1.75, {name = name..k, minZ = v.loc.z-1.50,maxZ = v.loc.z +1.5}, 
+			{ options = table, distance = 2.5})
+		elseif Config.Target == 'ox' then
+			exports.ox_target:addBoxZone({coords = v.loc, size = vec3(1,1,1), options = table })
+		end
+	end
+
+
 function AddSingleModel(model, data, num)
 	if Config.Target == 'qb' then
 		exports['qb-target']:AddTargetEntity(model, {options = {
